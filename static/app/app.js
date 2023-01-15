06455ea3120019ef78a45f26b4e27459ff540009
Md-Mosabbir <kmosabbir@gmail.com>
2023-01-15 14:58:31 +0600

    tic-tac-toe formed

--- static/app/app.js
+++ static/app/app.js
@@ -1,1 +1,19 @@
+// gameBoard
 
+(function gameBoard () {
+  const Board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'x', 'o']
+
+
+  // Displaying Board
+
+  const containerDiv = document.querySelector('.container')
+  // Function for displaying X and O in DOM
+
+}())
+
+// Factory Func for players
+
+function player (name, symbol) {
+  return { name, symbol }
+}
+const mosabbir = player('Mosabbir', 'X')
