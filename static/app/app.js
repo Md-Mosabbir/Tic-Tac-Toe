eed7484faf28e58ffc07f03dd77f705f64732f53
Md-Mosabbir <kmosabbir@gmail.com>
2023-01-17 07:59:48 +0600

    Game made Modular

--- static/app/app.js
+++ static/app/app.js
@@ -1,37 +1,59 @@
 // gameBoard
 
-
-(function gameBoard () {
-  const Board = ['', '', '', '', '', '', '', '', '']
-  let xMark = true
-
-  // Displaying Board
+const gameBoard = (function () {
+  let _Board = ['', '', '', '', '', '', '', '', '']
+  
 
   const boxCells = document.querySelectorAll('[data-box]')
   // Function for displaying X and O in DOM
   function displayArray () {
-    for (let i = 0; i < Board.length; i++) {
-      boxCells[i].textContent = Board[i]
+    for (let i = 0; i < _Board.length; i++) {
+      boxCells[i].textContent = _Board[i]
     }
   }
   displayArray()
 
+
+  return { 
+    displayArray,
+    
+    splicingBoard: function (data ,x){
+      _Board.splice(parseInt(data), 1, x)
+
+    }
+  }
+
+
+})()
+
+const displayController = (function () {
+  let _xMark = true
+  const boxCells = document.querySelectorAll('[data-box]')
+
   boxCells.forEach((item) => item.addEventListener('click', handleGame))
 
   function handleGame (e) {
-    if (e.target.textContent === '' && xMark === true) {
+    if (e.target.textContent === '' && _xMark === true) {
       e.target.textContent = mosabbir.symbol
-      Board.splice(parseInt(e.target.dataset.box), 1, mosabbir.symbol)
-      xMark = false
-    } else if (e.target.textContent === '' && xMark === false) {
+      gameBoard.splicingBoard(e.target.dataset.box ,mosabbir.symbol)
+      
+      _xMark = false
+    } else if (e.target.textContent === '' && _xMark === false) {
       e.target.textContent = mosarrat.symbol
-      Board.splice(parseInt(e.target.dataset.box), 1, mosarrat.symbol)
-      xMark = true
+      gameBoard.splicingBoard(e.target.dataset.box ,mosarrat.symbol)
+      _xMark = true
     }
   }
-}())
 
-// Factory Func for players
+})()
+
+// (function gameBoard () {
+//   // Displaying Board
+
+
+// }())
+
+// // Factory Func for players
 
 function player (name, symbol) {
   return { name, symbol }
